bf0130104e6da4198cad1e6a55c1f6b0d5103ef2
yuki <tokuakeyuki@gmail.com>
2021-03-15 04:40:07 +0000

    Add React Testing Library

--- src/__tests__/Sample.test.tsx
+++ src/__tests__/Sample.test.tsx
@@ -1,8 +1,19 @@
 /// <reference types="jest" />
 
 import React from 'react'
+import { cleanup, render, screen } from '@testing-library/react'
 import Home from 'pages/index'
 
 it('Home ページコンポーネントが存在している', () => {
   expect(Home).toBeTruthy()
+})
+
+// 各テスト実行後にレンダーしたコンポーネントをアンマウントする
+afterEach(cleanup)
+
+it('「Next.js!」のリンクが Next.js の公式サイトのトップページである', () => {
+  render(<Home />)
+  expect(screen.getByText('Next.js!').getAttribute('href')).toBe(
+    'https://nextjs.org'
+  )
 })
